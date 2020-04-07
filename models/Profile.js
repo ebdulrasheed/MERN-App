5c27de765f9b74681cad86502dbe36072e5b7174
Abdul Rasheed <ebdulrasheed@gmail.com>
2020-04-08 02:49:35 +0500

    [Major]: Created profile model

---
+++ models/Profile.js
@@ -0,0 +1,121 @@
+const mongoose = require("mongoose");
+const UserModel = require("./user");
+
+const ProfileSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.SchemaTypes.ObjectId,
+        ref: 'user'
+    },
+    company: {
+        type: String,
+        // required: true
+    },
+    website: {
+        type: String,
+        // required: true,
+        // unique: true
+    },
+    location: {
+        type: String,
+        // required: true
+    },
+    status: {
+        type: String,
+        required: true
+    },
+    skills: {
+        type: [String],
+        required: true
+    },
+    bio: {
+        type: String,
+    },
+    githubUsername: {
+        type: String,
+        required: true
+    },
+    experience: [
+        {
+            title: {
+                type: String,
+                required: true
+            },
+            company: {
+                type: String,
+                required: true
+            },
+            location: {
+                type: [String],
+                // required: true
+            },
+            from: {
+                type: Date,
+                required: true
+            },
+            to: {
+                type: Date
+            },
+            current: {
+                type: Boolean,
+                default: false
+            },
+            description: {
+                type: String
+            }
+        }
+    ],
+    education: [
+        {
+            school: {
+                type: String,
+                required: true
+            },
+            degree: {
+                type: String,
+                required: true
+            },
+            fieldOfStudy: {
+                type: String,
+                required: true
+            },
+            from: {
+                type: Date,
+                required: true
+            },
+            to: {
+                type: Date
+            },
+            current: {
+                type: Boolean,
+                default: false
+            },
+            description: {
+                type: String
+            }
+        }
+    ],
+    social: {
+        youtube: {
+            type: String
+        },
+        twitter: {
+            type: String
+        },
+        facebook: {
+            type: String
+        },
+        linkedin: {
+            type: String
+        },
+        instagram: {
+            type: String
+        }
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+}
+);
+
+module.exports = Profile = mongoose.model('profile', ProfileSchema);
